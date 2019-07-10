e5c1fcb3ae8c94e9341ea6854025df95898ec34b
Willtrigo <willtrigo@gmail.com>
2019-07-10 17:14:54 -0300

    should show changes to functional_tests.py, home.html, tests.py and views.py

--- lists/tests.py
+++ lists/tests.py
@@ -12,11 +12,17 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         """Verify html of home page."""
-        response = self.client.get('/')
+        response = self.client.get('/', data={'item_text': 'A new list item'})
 
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
 
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        """Save POST in the home page."""
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
